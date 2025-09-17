723a701108c5987c0ae14ed28d4428ed83a56a45
Ozoda <aminjonovaozoda596@gmail.com>
2025-09-17 14:27:52 +0500

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,6 +4,8 @@ if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL, ensure the database is provisioned");
 }
 
+console.log("DATABASE_URL:", process.env.DATABASE_URL); // Test uchun
+
 export default defineConfig({
   out: "./migrations",
   schema: "./shared/schema.ts",
